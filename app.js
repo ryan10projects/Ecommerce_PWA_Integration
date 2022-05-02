f112c9f19e3c0a17a9c9a03e48889f06a75b9785
ryan10projects <59692861+ryan10projects@users.noreply.github.com>
2022-05-02 12:15:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js')
   .then(function(registration) {
+    // Then later, request a one-off sync:
     console.log('Registration successful, scope is:', registration.scope);
+    navigator.serviceWorker.ready.then(function(swRegistration) {
+    return swRegistration.sync.register('myFirstSync');
   })
   .catch(function(error) {
     console.log('Service worker registration failed, error:', error);
   });
+  
+
+});
 }
